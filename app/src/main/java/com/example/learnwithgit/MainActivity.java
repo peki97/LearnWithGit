da552267739afc3fe7db9a13c5f4fa5694a48229
Peter-Christian Kizina <pkizina@compnetgmbh.de>
2021-07-30 09:15:14 +0200

    comments added || rebase cConflict -> master

--- app/src/main/java/com/example/learnwithgit/MainActivity.java
+++ app/src/main/java/com/example/learnwithgit/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.learnwithgit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+<<<<<<< HEAD
         //hafklahfklasdklf
+=======
+        Log.d("hdf", "hallo Weltus");
+        //asfhasjkfhasjkldfhl
+>>>>>>> 54135cf (comments added)
     }
 }
